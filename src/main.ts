4510f0590ab013439ca5fabc1456e4a05f89b95a
Max-TeXnic <max.cyberia@gmail.com>
2025-04-13 15:41:05 +0800

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  
   app.use(cookieParser());
   const config = new DocumentBuilder()
   .setTitle('Visiteo API')
