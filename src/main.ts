ca37a18f9121abde1e6be79d57bfb03e6b91bb49
Max-TeXnic <max.cyberia@gmail.com>
2025-04-13 15:47:17 +0800

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3001', // Разрешаем только этот домен
+    credentials: true, // Разрешаем передачу куки
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // Разрешаем только эти методы
+    allowedHeaders: ['Content-Type'], // Разрешаем только этот заголовок
+    exposedHeaders: ['Set-Cookie'], // Разрешаем передачу куки
+  });
   
   app.use(cookieParser());
   const config = new DocumentBuilder()
