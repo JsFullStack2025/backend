b122ec034aa14529bf28299e39345798d6ec09d9
Max-TeXnic <max.cyberia@gmail.com>
2025-05-20 21:36:33 +0800

    Added MinIO api

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,14 +9,17 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AuthController } from './auth/auth.controller'
 import { AuthService } from './auth/auth.service';
+//import { MinioModule } from './services/minio/minio.module';
+import { MinioController } from './services/minio/minio.controller';
+import { MinioService } from './services/minio/minio.service';
 //import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [
     AuthModule,
-    UsersModule,
+    UsersModule    
     ],
-  controllers: [AppController, AuthController],
-  providers: [AuthService, AppService, PrismaService, UsersService, CardsService, CardTypesService],
+  controllers: [AppController, AuthController, MinioController],
+  providers: [AuthService, MinioService, AppService, PrismaService, UsersService, CardsService, CardTypesService, MinioService],
 })
 export class AppModule {}
